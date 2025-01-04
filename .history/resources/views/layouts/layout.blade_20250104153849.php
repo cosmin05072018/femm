f08f58e125a0e4e3627ad282ab6ca38a33a30e17
Morari Cosmin <cosminmorari99@yahoo.com>
2025-01-04 15:40:39 +0200

    adaugam editare hotel

--- .history/resources/views/layouts/layout.blade_20250104153849.php
+++ .history/resources/views/layouts/layout.blade_20250104153849.php
@@ -240,15 +240,6 @@
     -->
     <script src="{{ asset('js/demo/datatables-demo.js') }}"></script>
     <script src="{{ asset('js/custom.js') }}"></script>
-
-    <script>
-        // Selectăm elementul #accordionSidebar
-        var sidebar = document.getElementById('accordionSidebar');
-
-        // Adăugăm clasa toggled pe click sau pe schimbarea dimensiunii ecranului
-        if (window.innerWidth <= 768) {
-            sidebar.classList.add('toggled');
-        } < /body>
-    </script>
+</body>
 
 </html>
--- .history/resources/views/layouts/layout.blade_20250104154021.php
+++ .history/resources/views/layouts/layout.blade_20250104154021.php
@@ -241,14 +241,10 @@
     <script src="{{ asset('js/demo/datatables-demo.js') }}"></script>
     <script src="{{ asset('js/custom.js') }}"></script>
 
-    <script>
-        // Selectăm elementul #accordionSidebar
-        var sidebar = document.getElementById('accordionSidebar');
+    // Selectăm elementul #accordionSidebar
+    var sidebar = document.getElementById('accordionSidebar');
 
-        // Adăugăm clasa toggled pe click sau pe schimbarea dimensiunii ecranului
-        if (window.innerWidth <= 768) {
-            sidebar.classList.add('toggled');
-        } < /body>
-    </script>
+    // Adăugăm clasa toggled pe click sau pe schimbarea dimensiunii ecranului
+    if (window.innerWidth <= 768) { sidebar.classList.add('toggled'); } </body>
 
 </html>
