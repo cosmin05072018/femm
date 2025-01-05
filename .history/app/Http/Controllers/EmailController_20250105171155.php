f79308a774e2b6f3722fa26150ae558982378af5
Morari Cosmin <cosminmorari99@yahoo.com>
2025-01-05 17:13:27 +0200

    start

---
+++ .history/app/Http/Controllers/EmailController_20250105171155.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Routing\Controller;
+
+class EmailController extends Controller
+{
+    public function readEmail()
+    {
+
+    }
+}
