7d14b7a63600e012981d9435cb8bfca68558c10e
Morari Cosmin <cosminmorari99@yahoo.com>
2025-01-05 12:20:16 +0200

    adaugam editare hotel

--- .history/resources/views/superAdmin/usermanagement.blade_20241228140656.php
+++ .history/resources/views/superAdmin/usermanagement.blade_20241228140656.php
@@ -6,7 +6,7 @@
 <!-- DataTales Example -->
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Manageri de Hoteluri</h6>
+            <h6 class="m-0 font-weight-bold text-primary">Utiilizatori</h6>
         </div>
         <div class="card-body">
             <div class="table-responsive">
