8c8d5db009cf0e56dd5881c9abaaa71ca209a6aa
Cosmin <cosminmorari99@yahoo.com>
2025-01-06 14:46:34 +0200

    test

--- .history/public/js/custom_20250104154333.js
+++
@@ -1,7 +0,0 @@
-// Selectăm elementul #accordionSidebar
-var sidebar = document.getElementById('accordionSidebar');
-console.log(sidebar);
-// Adăugăm clasa toggled pe click sau pe schimbarea dimensiunii ecranului
-if (window.innerWidth <= 768) {
-    sidebar.classList.add('toggled');
-}
--- .history/resources/views/superAdmin/hotel.blade_20250104132653.php
+++
@@ -1,3 +0,0 @@
-@foreach ($data as $a)
-
-@endforeach
