0074c4834ebf077b44289d24fc4a9c4aca33049d
Cosmin <cosminmorari99@yahoo.com>
2025-01-26 16:54:38 +0200

    asd

--- resources/views/superAdmin/view-email.blade.php
+++ resources/views/superAdmin/view-email.blade.php
@@ -8,10 +8,10 @@
 
         <!-- Main Content -->
         <div id="content">
-            <h1>Messages from: {{ $mailAddressView }}</h1>
+            <h1>Messages from: {{ $mailAdressView }}</h1>
 
             @if ($messages->isEmpty())
-                <p>No messages found from {{ $mailAddressView }}.</p>
+                <p>No messages found from {{ $mailAdressView }}.</p>
             @else
                 <table class="table">
                     <thead>
