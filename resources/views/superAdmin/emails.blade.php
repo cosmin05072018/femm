e358d712729eba7898b2405062b16f34e6e15dbe
Cosmin <cosminmorari99@yahoo.com>
2025-01-26 11:55:10 +0200

    asd

--- resources/views/superAdmin/emails.blade.php
+++ resources/views/superAdmin/emails.blade.php
@@ -8,10 +8,23 @@
 
         <!-- Main Content -->
         <div id="content">
-            <div class="py-5">
-                <h1>Emailuri</h1>
-                {{ $messages }}
-            </div>
+            <h1>Emailuri</h1>
+            <ul>
+                @foreach ($messages as $message)
+                    <li>
+                        <strong>{{ $message->getSubject() }}</strong><br>
+                        De la: {{ $message->getFrom()[0]->mail }}<br>
+                        Data: {{ $message->getDate() }}<br>
+                        <a href="{{ url('/emails/' . $message->getUid()) }}">Deschide</a> |
+                        <form action="{{ url('/emails/1/reply') }}" method="POST">
+                            @csrf
+                            <textarea name="message" required></textarea>
+                            <button type="submit">Răspunde</button>
+                        </form>
+
+                    </li>
+                @endforeach
+            </ul>
         </div>
     </div>
 @endsection
