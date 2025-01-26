16ddaff569f8a4a759d0b8ed4f112263a4e9a3fb
Cosmin <cosminmorari99@yahoo.com>
2025-01-26 12:40:04 +0200

    asd

--- resources/views/superAdmin/emails.blade.php
+++ resources/views/superAdmin/emails.blade.php
@@ -51,20 +51,19 @@
                         <!-- tabs -->
                         <ul class="nav nav-tabs border-0">
                             <li class="nav-item">
-                                <a href="#" class="btn btn-danger btn-sm btn-block" role="button"><i class="fa fa-edit"></i> Compose</a>
-                            </li>
-                            <li class="nav-item">
                                 <a class="nav-link active" href="#inbox" data-toggle="tab">
                                     <i class="fa fa-inbox mr-1"></i> Toate mesajele
                                 </a>
                             </li>
                             <li><a href="#profile" class="nav-link" data-toggle="tab">
-                                <i class="fa fa-user mr-1"></i> Mesaje Trimise </a></li>
+                                    <i class="fa fa-user mr-1"></i> Mesaje Trimise </a></li>
                             <li><a href="#settings" class="nav-link" data-toggle="tab">
-                                <i class="fa fa-plus"></i>
+                                    <i class="fa fa-plus"></i>
                                 </a>
                             </li>
                         </ul>
+                        <a href="#" class="btn btn-danger btn-sm btn-block" role="button"><i class="fa fa-edit"></i>
+                            Compune un mail</a>
                         <!-- tab panes -->
                         <div class="tab-content py-4">
                             <div class="tab-pane in active" id="inbox">
@@ -78,8 +77,10 @@
                                                 </btn>
                                             </td>
                                             <td>
-                                                <button class="btn btn-light"><i title="delete selected" class="fa fa-trash"></i></button>
-                                                <button class="btn btn-light"><i title="move to folder" class="fa fa-folder-open"></i></button>
+                                                <button class="btn btn-light"><i title="delete selected"
+                                                        class="fa fa-trash"></i></button>
+                                                <button class="btn btn-light"><i title="move to folder"
+                                                        class="fa fa-folder-open"></i></button>
                                             </td>
                                             <td></td>
                                         </tr>
@@ -88,26 +89,34 @@
                                             <td>
                                                 <label>
                                                     <input type="checkbox">
-                                                </label> <span class="name text-truncate">Mark Otto</span></td>
-                                            <td><span class="subject">Nice work on the docs for lastest version</span> <small class="text-muted">- Joe, I just reviewed the last...</small></td>
-                                            <td><span class="badge">12:10 AM</span> <span class="float-right fa fa-paperclip"></span></td>
+                                                </label> <span class="name text-truncate">Mark Otto</span>
+                                            </td>
+                                            <td><span class="subject">Nice work on the docs for lastest version</span>
+                                                <small class="text-muted">- Joe, I just reviewed the last...</small></td>
+                                            <td><span class="badge">12:10 AM</span> <span
+                                                    class="float-right fa fa-paperclip"></span></td>
                                         </tr>
                                         <!-- inbox item -->
                                         <tr>
                                             <td>
                                                 <label>
                                                     <input type="checkbox">
-                                                </label> <span class="name text-truncate">Anil Judah</span></td>
-                                            <td><span class="subject">GAE Project</span> <small class="text-muted">- Can you take a second to look..</small></td>
-                                            <td><span class="badge badge-inverse">11:33 AM</span> <span class="float-right fa fa-warning-sign text-danger"></span></td>
+                                                </label> <span class="name text-truncate">Anil Judah</span>
+                                            </td>
+                                            <td><span class="subject">GAE Project</span> <small class="text-muted">- Can you
+                                                    take a second to look..</small></td>
+                                            <td><span class="badge badge-inverse">11:33 AM</span> <span
+                                                    class="float-right fa fa-warning-sign text-danger"></span></td>
                                         </tr>
                                         <!-- inbox item -->
                                         <tr class="unread">
                                             <td>
                                                 <label>
                                                     <input type="checkbox">
-                                                </label> <span class="name text-truncate">Terry Lincoln</span></td>
-                                            <td><span class="subject">Vacation pics</span> <small class="text-muted">(this message contains images)</small></td>
+                                                </label> <span class="name text-truncate">Terry Lincoln</span>
+                                            </td>
+                                            <td><span class="subject">Vacation pics</span> <small class="text-muted">(this
+                                                    message contains images)</small></td>
                                             <td><span class="badge">11:13 AM</span> <span class="float-right"></span></td>
                                         </tr>
                                         <!-- inbox item -->
@@ -115,8 +124,11 @@
                                             <td>
                                                 <label>
                                                     <input type="checkbox">
-                                                </label> <span class="name text-truncate">Mark Brown</span></td>
-                                            <td><span class="subject">Last call for this weekend</span> <small class="text-muted">- Hi Joe, Thanks for sending over those..</small></td>
+                                                </label> <span class="name text-truncate">Mark Brown</span>
+                                            </td>
+                                            <td><span class="subject">Last call for this weekend</span> <small
+                                                    class="text-muted">- Hi Joe, Thanks for sending over those..</small>
+                                            </td>
                                             <td><span class="badge">11:05 AM</span> <span class="pull-right"></span></td>
                                         </tr>
                                         <!-- inbox item -->
@@ -124,18 +136,24 @@
                                             <td class=" text-truncate">
                                                 <label>
                                                     <input type="checkbox">
-                                                </label> <span class="name text-truncate">Jorge Anodonolgez</span></td>
-                                            <td><span class="subject">Meeting with Simco</span> <small class="text-muted">- Joe I included your contact info for the...</small></td>
-                                            <td><span class="badge">10:54 AM</span> <span class="pull-right glyphicon glyphicon-paperclip"></span></td>
+                                                </label> <span class="name text-truncate">Jorge Anodonolgez</span>
+                                            </td>
+                                            <td><span class="subject">Meeting with Simco</span> <small class="text-muted">-
+                                                    Joe I included your contact info for the...</small></td>
+                                            <td><span class="badge">10:54 AM</span> <span
+                                                    class="pull-right glyphicon glyphicon-paperclip"></span></td>
                                         </tr>
                                         <!-- inbox item -->
                                         <tr>
                                             <td class="text-nowrap">
                                                 <label>
                                                     <input type="checkbox">
-                                                </label> <span class="name text-truncate">Mark Otto</span></td>
-                                            <td><span class="subject">FYI: New Release</span> <small class="text-muted">this message is high priority</small></td>
-                                            <td><span class="badge badge-dark">9:58 AM</span> <span class="float-right"></span></td>
+                                                </label> <span class="name text-truncate">Mark Otto</span>
+                                            </td>
+                                            <td><span class="subject">FYI: New Release</span> <small class="text-muted">this
+                                                    message is high priority</small></td>
+                                            <td><span class="badge badge-dark">9:58 AM</span> <span
+                                                    class="float-right"></span></td>
                                         </tr>
                                     </tbody>
                                 </table>
