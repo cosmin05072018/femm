0f407249d9a01032a1a5c01d72015d36af16d7c9
Cosmin <cosminmorari99@yahoo.com>
2025-01-26 16:08:58 +0200

    asd

--- app/Http/Controllers/SuperAdmin/EmailsController.php
+++ app/Http/Controllers/SuperAdmin/EmailsController.php
@@ -54,8 +54,9 @@ class EmailsController extends Controller
         foreach ($messages as $message) {
             $flags = $message->flags();  // Obține colecția de flag-uri
             $message->is_seen = $flags->contains('Seen');  // Verifică dacă colecția conține flag-ul 'Seen'
+            dd($message->bodies());
         }
-        dd($messages->bodies());
+
 
         // $idUserFromMail = User::where('email_femm', $account)->value('id');
 
