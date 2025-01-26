c61c93d56eb7bceb3417ce9313b71e90cb7be8d3
Cosmin <cosminmorari99@yahoo.com>
2025-01-26 13:50:31 +0200

    asd

--- app/Http/Controllers/SuperAdmin/EmailsController.php
+++ app/Http/Controllers/SuperAdmin/EmailsController.php
@@ -50,7 +50,7 @@ class EmailsController extends Controller
 
         $messages = $inbox->messages()->all()->get();
 
-        dd($messages->seen()->get());
+        dd($inbox);
 
 
         return view('superAdmin/emails', compact('owner', 'messages'));
