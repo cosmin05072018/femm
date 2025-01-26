ff0227e87f61a9af13ec2f12535218ff99bc9d09
Cosmin <cosminmorari99@yahoo.com>
2025-01-26 20:37:35 +0200

    asd

--- resources/views/superAdmin/view-email.blade.php
+++ resources/views/superAdmin/view-email.blade.php
@@ -13,68 +13,28 @@
 
             <p class="text-dark">Continutul mailului:</p>
             <div class="container-fluid p-3 border b-rounded">
-                @php
-    $messageBody = $messages->getHTMLBody();
-
-    $messageBody = preg_replace_callback('/src="cid:([^"]+)"/', function ($matches) {
-        $contentId = $matches[1];
-        return 'src="' . asset('storage/emails/' . $contentId . '.jpg') . '"';
-    }, $messageBody);
-
-    dd($messageBody); // Verifică rezultatul procesării
-@endphp
-
+                {!! $messages->getHTMLBody() !!}
             </div>
 
-            @php
-                // Obține atașamentele mesajului
-                $attachments = $messages->getAttachments();
-            @endphp
-
-            @if ($attachments->count() > 0)
-                <div class="mt-4">
-                    <h5>Atașamente:</h5>
-                    <ul>
-                        @foreach ($attachments as $attachment)
-                            @php
-                                // Salvează atașamentele într-un folder temporar (public/storage/emails)
-                                $filePath = storage_path('app/public/emails/' . $attachment->name);
-                                $attachment->save(storage_path('app/public/emails'));
-                            @endphp
-                            <li>
-                                <a href="{{ asset('storage/emails/' . $attachment->name) }}" target="_blank">
-                                    {{ $attachment->name }}
-                                </a>
-                            </li>
-                        @endforeach
-                    </ul>
-                </div>
-            @else
-                <p class="text-muted">Nu există atașamente în acest e-mail.</p>
-            @endif
             <!-- Butonul pentru activarea modalului -->
-            <a href="#" class="btn btn-info btn-sm my-4" title="Raspunde" data-bs-toggle="modal"
-                data-bs-target="#userRequestModal">
+            <a href="#" class="btn btn-info btn-sm my-4" title="Raspunde" data-bs-toggle="modal" data-bs-target="#userRequestModal">
                 Raspunde
             </a>
 
             <!-- Modalul complet -->
-            <div class="modal fade" id="userRequestModal" tabindex="-1" aria-labelledby="userRequestModalLabel"
-                aria-hidden="true">
+            <div class="modal fade" id="userRequestModal" tabindex="-1" aria-labelledby="userRequestModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="userRequestModalLabel">Raspunde lui
-                                <b>{{ $messages->getFrom()[0]->mail }}</b></h5>
+                            <h5 class="modal-title" id="userRequestModalLabel">Raspunde lui <b>{{ $messages->getFrom()[0]->mail }}</b></h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Închide"></button>
                         </div>
                         <div class="modal-body">
-                            <form action="{{ route('admin.reply', ['email' => $messages->getUid()]) }}" method="POST"
-                                style="display:inline;">
+                            <form action="{{ route('admin.reply', ['email' => $messages->getUid()]) }}" method="POST" style="display:inline;">
                                 @csrf
                                 <div class="mb-3">
                                     <textarea class="form-control" id="exampleFormControlTextarea1" name="reply_message" rows="8"></textarea>
-                                </div>
+                                  </div>
                                 <button type="submit" class="btn btn-primary">Trimite</button>
                             </form>
                         </div>
