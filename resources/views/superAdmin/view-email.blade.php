bb9c666d7f69c27685e477ec44e694a906a70b53
Cosmin <cosminmorari99@yahoo.com>
2025-01-26 17:38:56 +0200

    asd

--- resources/views/superAdmin/view-email.blade.php
+++ resources/views/superAdmin/view-email.blade.php
@@ -2,17 +2,23 @@
 
 @section('title', 'Emailuri')
 
+<style>
+    .content-mail{
+        box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px !important;
+    }
+</style>
+
 @section('content')
     <!-- Content Wrapper -->
     <div id="content-wrapper" class="d-flex flex-column">
 
         <!-- Main Content -->
         <div id="content">
-            <p>Mail primit de la: <b>{{ $messages->getFrom()[0]->mail }}</b></p>
-            <p>Subiect: <b>{{ $messages->getSubject() }}</b></p>
+            <p class="text-dark">Mail primit de la: <b>{{ $messages->getFrom()[0]->mail }}</b></p>
+            <p class="text-dark">Subiect: <b>{{ $messages->getSubject() }}</b></p>
 
-            <h4>Continutul mailului:</h4>
-            <div class="container-fluid p-3">
+            <p class="text-dark">Continutul mailului:</p>
+            <div class="container-fluid p-3 content-mail">
                 {!! $messages->getHTMLBody() !!}
             </div>
         </div>
