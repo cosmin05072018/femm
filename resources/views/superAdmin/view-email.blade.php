b762a5b7ac16b08ba91a8ed1ee28b931694d420e
Cosmin <cosminmorari99@yahoo.com>
2025-01-26 21:02:53 +0200

    asd

--- resources/views/superAdmin/view-email.blade.php
+++ resources/views/superAdmin/view-email.blade.php
@@ -8,8 +8,7 @@
 
         <!-- Main Content -->
         <div id="content mt-5">
-            <p class="text-dark">Mail primit de la: <b>{{ $messages->getFrom()[0]->mail }}</b></p>
-            <p class="text-dark">Subiect: <b>{{ $messages->getSubject() }}</b></p>
+
 
             <p class="text-dark">Continutul mailului:</p>
             <div class="container-fluid p-3 border b-rounded">
@@ -21,26 +20,7 @@
                 Raspunde
             </a>
 
-            <!-- Modalul complet -->
-            <div class="modal fade" id="userRequestModal" tabindex="-1" aria-labelledby="userRequestModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="userRequestModalLabel">Raspunde lui <b>{{ $messages->getFrom()[0]->mail }}</b></h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Închide"></button>
-                        </div>
-                        <div class="modal-body">
-                            <form action="{{ route('admin.reply', ['email' => $messages->getUid()]) }}" method="POST" style="display:inline;">
-                                @csrf
-                                <div class="mb-3">
-                                    <textarea class="form-control" id="exampleFormControlTextarea1" name="reply_message" rows="8"></textarea>
-                                  </div>
-                                <button type="submit" class="btn btn-primary">Trimite</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
+
         </div>
     </div>
 @endsection
