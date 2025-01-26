6a2712900ab3f396b5f74b8c2f48004000edf204
Cosmin <cosminmorari99@yahoo.com>
2025-01-26 17:22:17 +0200

    asd

--- resources/views/superAdmin/view-email.blade.php
+++ resources/views/superAdmin/view-email.blade.php
@@ -10,7 +10,7 @@
         <div id="content">
             <h1>Messages from: {{ $mailAdressView }}</h1>
 
-            {{ $messages->getDate() }}
+            {{ $messages->getTextBody() }}
         </div>
     </div>
 @endsection
