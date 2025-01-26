92f010332ff0ab3b10a1741095c0373cb289c499
Cosmin <cosminmorari99@yahoo.com>
2025-01-26 12:07:22 +0200

    asd

--- resources/views/superAdmin/emails.blade.php
+++ resources/views/superAdmin/emails.blade.php
@@ -15,7 +15,7 @@
                         <strong>{{ $message->getSubject() }}</strong><br>
                         De la: {{ $message->getFrom()[0]->mail }}<br>
                         Data: {{ $message->getDate() }}<br>
-                        <p>Conținut: {{ $message->bodies() }}</p>
+                        <p>Conținut: {{ $message->bodies}}</p>
                     </li>
                 @endforeach
             </ul>
