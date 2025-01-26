16498bf64d1ab7fc17fd4b3aa8647bfe56402e91
Cosmin <cosminmorari99@yahoo.com>
2025-01-26 11:51:56 +0200

    asd

--- resources/views/superAdmin/emails.blade.php
+++ resources/views/superAdmin/emails.blade.php
@@ -10,8 +10,12 @@
         <div id="content">
             <div class="py-5">
                 <h1>Emailuri</h1>
-                @foreach ($messages as $message)
-                    {{ $message }}
+                    @foreach ($messages as $email)
+                    <tr>
+                        <td>{{ $email['subject'] }}</td>
+                        <td>{{ $email['from_email'] }}</td> <!-- Afișează doar adresa de e-mail -->
+                        <td>{{ $email['body'] }}</td>
+                    </tr>
                 @endforeach
             </div>
         </div>
