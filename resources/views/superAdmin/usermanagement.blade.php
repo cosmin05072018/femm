93e1c23438c03a47f98265d904a76836458b412e
Cosmin <cosminmorari99@yahoo.com>
2025-02-08 13:39:18 +0200

    first commit

--- resources/views/superAdmin/usermanagement.blade.php
+++ resources/views/superAdmin/usermanagement.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <!-- DataTales Example -->
-    <div class="card shadow mb-4">
+    <div class="card card-custom-users shadow mb-4">
         <div class="card-header py-3">
             <h6 class="m-0 font-weight-bold text-primary">Manageri de Hoteluri</h6>
         </div>
