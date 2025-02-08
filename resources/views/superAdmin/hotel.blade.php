410edba1ddee5f16db8fd31d2006081dc0d60d2a
Cosmin <cosminmorari99@yahoo.com>
2025-02-08 20:43:20 +0200

    first commit

--- resources/views/superAdmin/hotel.blade.php
+++ resources/views/superAdmin/hotel.blade.php
@@ -3,6 +3,12 @@
 @section('title', 'Hotel')
 
 @section('content')
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Bootstrap Bundle JS (include și Popper.js) -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
     <style>
         .card-custom:hover {
             box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px !important;
