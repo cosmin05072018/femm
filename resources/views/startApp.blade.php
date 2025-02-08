2d1f367a67d217e7f21d79116ab7b4fc06d35823
Cosmin <cosminmorari99@yahoo.com>
2025-02-08 13:15:00 +0200

    first commit

--- resources/views/startApp.blade.php
+++ resources/views/startApp.blade.php
@@ -837,6 +837,7 @@
     <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link href="{{ asset('css/custom-design.css') }}" rel="stylesheet">
 </head>
 
 <body>
