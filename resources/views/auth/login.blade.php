66b72ff2ac2e401fe37d4b5c695549e890effe4f
Cosmin <cosminmorari99@yahoo.com>
2025-02-08 11:19:09 +0200

    first commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,12 +38,14 @@
                     class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
                     name="remember">
                 <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Ține-mă minte') }}</span>
+            </label>
+            <label class="inline-flex items-center">
                 <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                        href="{{ route('password.request') }}">
-                        {{ __('Ți-ai uitat parola?') }}
-                    </a>
+                    @if (Route::has('password.request'))
+                        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                            href="{{ route('password.request') }}">
+                            {{ __('Ți-ai uitat parola?') }}
+                        </a>
                 @endif
                 </span>
             </label>
