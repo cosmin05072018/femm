6fc50da137be9bbb9aec78ca1e1f2ac1b71e512d
Cosmin <cosminmorari99@yahoo.com>
2025-02-08 14:27:40 +0200

    first commit

--- resources/views/superAdmin/usermanagement.blade.php
+++ resources/views/superAdmin/usermanagement.blade.php
@@ -19,6 +19,7 @@
                             <th>Email</th>
                             <th>Status Cont</th>
                             <th>Rol</th>
+                            <th>Departament</th>
                             <th>Hotel</th>
                             <th>Actiuni</th>
                         </tr>
@@ -37,6 +38,7 @@
                                 {{ $user->status == 0 ? 'În așteptare' : ($user->status == 1 ? 'Aprobat' : 'Respins') }}
                             </td>
                             <td class='{{ $user->role ? '' : 'text-danger' }}'>{{ $user->role ? $user->role : 'Nu sunt informatii' }}</td>
+                            <td class='{{ $user->department_id ? '' : 'text-danger' }}'>{{ $user->department ? $user->department->name : 'Nu sunt informatii' }}</td>
                             <td class='{{ $user->hotel_name ? '' : 'text-danger' }}'>
                                 {{ $user->hotel_name ? $user->hotel_name : 'Nu sunt informatii' }}
                             </td>
