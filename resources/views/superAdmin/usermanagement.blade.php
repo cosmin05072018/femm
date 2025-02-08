add2ca243f97c1c1c5278296134af0f204797b92
Cosmin <cosminmorari99@yahoo.com>
2025-02-08 13:57:38 +0200

    first commit

--- resources/views/superAdmin/usermanagement.blade.php
+++ resources/views/superAdmin/usermanagement.blade.php
@@ -13,7 +13,7 @@
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead>
                         <tr>
-                            <th>Nr. Crt.</th>
+                            <th></th>
                             <th>Oras</th>
                             <th>Nume Manager</th>
                             <th>Email</th>
