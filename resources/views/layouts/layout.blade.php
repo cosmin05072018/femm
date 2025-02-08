de98e204d2aaec52ca8e859857bfa81ab8d9fabb
Cosmin <cosminmorari99@yahoo.com>
2025-02-08 12:44:59 +0200

    first commit

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -164,7 +164,7 @@
                 <div id="content-wrapper" class="d-flex flex-column">
                     <div id="content">
                         <!-- Main Content -->
-                        <div class="container-fluid">
+                        <div class="container-fluid custom-content">
                             @yield('content')
                         </div>
                     </div>
