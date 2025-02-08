a910b8cd9f0e9e3417661e0365d0c8ff61313c8d
Cosmin <cosminmorari99@yahoo.com>
2025-02-08 15:11:04 +0200

    first commit

--- resources/views/superAdmin/usermanagement.blade.php
+++ resources/views/superAdmin/usermanagement.blade.php
@@ -57,7 +57,7 @@
                                         <!-- Modalul complet -->
                                         <div class="modal fade" id="userRequestModal-{{ $user->id }}" tabindex="-1" aria-labelledby="userRequestModalLabel-{{ $user->id }}" aria-hidden="true">
                                             <div class="modal-dialog">
-                                                <div class="modal-content">
+                                                <div class="modal-content modal-custom">
                                                     <div class="modal-header">
                                                         <h5 class="modal-title" id="userRequestModalLabel-{{ $user->id }}">Detalii cerere utilizator</h5>
                                                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Închide"></button>
