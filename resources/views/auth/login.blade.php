73fa1b62e138515b51ed63e7083208c82a346048
Cosmin <cosminmorari99@yahoo.com>
2025-02-08 11:23:33 +0200

    first commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,7 +32,7 @@
         </div>
 
         <!-- Remember Me -->
-        <div class="flex mt-4 justify-content-between">
+        <div class="d-flex mt-4 justify-content-between">
             <label for="remember_me" class="items-center">
                 <input id="remember_me" type="checkbox"
                     class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"
