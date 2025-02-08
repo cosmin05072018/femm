df492c822d82e520d1f05083eef341bfa3d87c37
Cosmin <cosminmorari99@yahoo.com>
2025-02-08 11:59:38 +0200

    first commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -58,12 +58,12 @@
         </div>
 
         <div class="flex items-center justify-between pt-4 buttons-costom-links">
-            <x-primary-button class="ms-3">
+            <x-primary-button>
                 {{ __('Conecteză-te') }}
             </x-primary-button>
             @if (Route::has('register'))
                 <a href="{{ route('register') }}"
-                    class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Nu
+                    class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Nu
                     ai cont? Crează unul</a>
             @endif
         </div>
