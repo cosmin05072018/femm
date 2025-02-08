7507d2f080ce7afb08749c8b91bf97478b2264c4
Cosmin <cosminmorari99@yahoo.com>
2025-02-08 17:07:22 +0200

    first commit

--- resources/views/superAdmin/hotel.blade.php
+++ resources/views/superAdmin/hotel.blade.php
@@ -51,8 +51,7 @@
                             <p><span>Adresa Companie:</span> <b>{{ $user['company_address'] }}</b></p>
                         @endforeach
                         <!-- Button trigger modal -->
-                        <button type="button" class="btn btn-danger" data-bs-toggle="modal"
-                            data-bs-target="#exampleModal">
+                        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
                             Șterge hotel
                         </button>
 
@@ -70,7 +69,8 @@
                                         <p class="py-5">Ești sigur că vrei să ștergi acest utilizator?</p>
                                     </div>
                                     <div class="modal-footer d-flex justify-content-between">
-                                        <form action="{{ route('admin.hotel.destroy', $hotelSelected->id) }}" method="POST" class="mt-2">
+                                        <form action="{{ route('admin.hotel.destroy', $hotelSelected->id) }}" method="POST"
+                                            class="mt-2">
                                             @csrf
                                             @method('DELETE')
                                             <button type="submit" class="btn btn-danger">Șterge hotel</button>
@@ -85,7 +85,7 @@
                 </div>
             </div>
 
-            <div class="mt-5 card shadow mb-4 card-custom-hotel"  id="formular-angajati">
+            <div class="mt-5 card shadow mb-4 card-custom-hotel" id="formular-angajati">
                 <div class="card-header py-3 d-flex">
                     <h4 class="card-title">
                         <i class="fas fa-user-plus me-2 text-primary"></i> Adaugă angajați
@@ -96,110 +96,110 @@
                         <form action="{{ route('admin.hotel.create-user', ['id' => $hotelSelected->id]) }}" method="POST">
                             @csrf
 
-                            <!-- Input Nume -->
-                            <div class="mb-3 col-md-5 col-12 p-0">
-                                <label for="name" class="form-label">Nume</label>
-                                <input type="text" class="form-control @error('name') is-invalid @enderror"
-                                    id="name" name="name" placeholder="Introduceti numele"
-                                    value="{{ old('name') }}">
-                                @error('name')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
-                            </div>
+                            <div class="row">
+                                <!-- Input Nume -->
+                                <div class="mb-3 col-md-6 col-12">
+                                    <label for="name" class="form-label">Nume</label>
+                                    <input type="text" class="form-control @error('name') is-invalid @enderror"
+                                        id="name" name="name" placeholder="Introduceti numele"
+                                        value="{{ old('name') }}">
+                                    @error('name')
+                                        <div class="invalid-feedback">{{ $message }}</div>
+                                    @enderror
+                                </div>
 
-                            <!-- Input Număr de Telefon -->
-                            <div class="mb-3 col-md-5 col-12 p-0">
-                                <label for="phone" class="form-label">Număr de Telefon</label>
-                                <input type="tel" class="form-control @error('phone') is-invalid @enderror"
-                                    id="phone" name="phone" placeholder="Introduceti numărul de telefon"
-                                    value="{{ old('phone') }}">
-                                @error('phone')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
-                            </div>
+                                <!-- Input Număr de Telefon -->
+                                <div class="mb-3 col-md-6 col-12">
+                                    <label for="phone" class="form-label">Număr de Telefon</label>
+                                    <input type="tel" class="form-control @error('phone') is-invalid @enderror"
+                                        id="phone" name="phone" placeholder="Introduceti numărul de telefon"
+                                        value="{{ old('phone') }}">
+                                    @error('phone')
+                                        <div class="invalid-feedback">{{ $message }}</div>
+                                    @enderror
+                                </div>
 
-                            <!-- Input Email -->
-                            <div class="mb-3 col-md-5 col-12 p-0">
-                                <label for="email" class="form-label">Email</label>
-                                <input type="text" class="form-control @error('email') is-invalid @enderror"
-                                    id="email" name="email" placeholder="Introduceti adresa de email"
-                                    value="{{ old('email') }}">
-                                <small class="form-text text-muted">Introduceti doar numele. Adresa de email va fi
-                                    completată automat cu
-                                    @femm.ro</small>
-                                @error('email')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
-                            </div>
+                                <!-- Input Email -->
+                                <div class="mb-3 col-md-6 col-12">
+                                    <label for="email" class="form-label">Email</label>
+                                    <input type="text" class="form-control @error('email') is-invalid @enderror"
+                                        id="email" name="email" placeholder="Introduceti adresa de email"
+                                        value="{{ old('email') }}">
+                                    <small class="form-text text-muted">Introduceti doar numele. Adresa de email va fi
+                                        completată automat cu @femm.ro</small>
+                                    @error('email')
+                                        <div class="invalid-feedback">{{ $message }}</div>
+                                    @enderror
+                                </div>
 
-                            <!-- Input Parola -->
-                            <div class="mb-3 col-md-5 col-12 p-0">
-                                <label for="email" class="form-label">Parola</label>
-                                <input type="password" class="form-control @error('password') is-invalid @enderror"
-                                    id="password" name="password" placeholder="Introduceti parola"
-                                    value="{{ old('password') }}">
-                                @error('password')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
-                            </div>
+                                <!-- Input Parola -->
+                                <div class="mb-3 col-md-6 col-12">
+                                    <label for="password" class="form-label">Parola</label>
+                                    <input type="password" class="form-control @error('password') is-invalid @enderror"
+                                        id="password" name="password" placeholder="Introduceti parola"
+                                        value="{{ old('password') }}">
+                                    @error('password')
+                                        <div class="invalid-feedback">{{ $message }}</div>
+                                    @enderror
+                                </div>
 
-                            <!-- Select Sef Departament -->
-                            <div class="mb-3 col-md-5 col-12 p-0">
-                                <label for="role" class="form-label">Rol</label>
-                                <select class="form-select @error('role') is-invalid @enderror" id="role"
-                                    name="role">
-                                    <option value="0" disabled selected>-- Selectați rolul --</option>
-                                    @foreach ($roles as $role)
-                                        <option value="{{ $role->id }}"
-                                            {{ old('role') == $role->id ? 'selected' : '' }}>
-                                            @if ($role->name == 'admin')
-                                                Șef Departament
-                                            @elseif($role->name == 'user')
-                                                Angajat
-                                            @endif
-                                        </option>
-                                    @endforeach
-                                </select>
-                                @error('role')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
-                            </div>
+                                <!-- Select Rol -->
+                                <div class="mb-3 col-md-6 col-12">
+                                    <label for="role" class="form-label">Rol</label>
+                                    <select class="form-select @error('role') is-invalid @enderror" id="role"
+                                        name="role">
+                                        <option value="0" disabled selected>-- Selectați rolul --</option>
+                                        @foreach ($roles as $role)
+                                            <option value="{{ $role->id }}"
+                                                {{ old('role') == $role->id ? 'selected' : '' }}>
+                                                @if ($role->name == 'admin')
+                                                    Șef Departament
+                                                @elseif($role->name == 'user')
+                                                    Angajat
+                                                @endif
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                    @error('role')
+                                        <div class="invalid-feedback">{{ $message }}</div>
+                                    @enderror
+                                </div>
 
-                            <!-- Input Functie -->
-                            <div class="mb-3 col-md-5 col-12 p-0" id="functie-group">
-                                <label for="functie" class="form-label">Functie</label>
-                                <input type="text" class="form-control @error('functie') is-invalid @enderror"
-                                    id="functie" name="functie" placeholder="Introduceti numele functiei"
-                                    value="{{ old('functie') }}">
-                                @error('functie')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
-                            </div>
+                                <!-- Input Functie -->
+                                <div class="mb-3 col-md-6 col-12">
+                                    <label for="functie" class="form-label">Functie</label>
+                                    <input type="text" class="form-control @error('functie') is-invalid @enderror"
+                                        id="functie" name="functie" placeholder="Introduceti numele functiei"
+                                        value="{{ old('functie') }}">
+                                    @error('functie')
+                                        <div class="invalid-feedback">{{ $message }}</div>
+                                    @enderror
+                                </div>
 
-                            <!-- Select Departament -->
-                            <div class="mb-3 col-md-5 col-12 p-0">
-                                <label for="department" class="form-label">Departament</label>
-                                <select class="form-select @error('department') is-invalid @enderror" id="department"
-                                    name="department">
-                                    <option value="0" disabled selected>-- Selectați departamentul --</option>
-                                    @foreach ($departments as $department)
-                                        <option value="{{ $department->id }}"
-                                            {{ old('department') == $department->id ? 'selected' : '' }}>
-                                            {{ $department->name }}
-                                        </option>
-                                    @endforeach
-                                </select>
-                                @error('department')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
-                            </div>
+                                <!-- Select Departament -->
+                                <div class="mb-3 col-md-6 col-12">
+                                    <label for="department" class="form-label">Departament</label>
+                                    <select class="form-select @error('department') is-invalid @enderror" id="department"
+                                        name="department">
+                                        <option value="0" disabled selected>-- Selectați departamentul --</option>
+                                        @foreach ($departments as $department)
+                                            <option value="{{ $department->id }}"
+                                                {{ old('department') == $department->id ? 'selected' : '' }}>
+                                                {{ $department->name }}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                    @error('department')
+                                        <div class="invalid-feedback">{{ $message }}</div>
+                                    @enderror
+                                </div>
 
-                            <!-- Buton Submit -->
-                            <div class="mb-3 col-md-5 col-12 p-0">
-                                <button type="submit" class="btn btn-custom-action w-100">Salvează</button>
+                                <!-- Buton Submit -->
+                                <div class="mb-3 col-12">
+                                    <button type="submit" class="btn btn-custom-action w-100">Salvează</button>
+                                </div>
                             </div>
                         </form>
-
                     </div>
                 </div>
             </div>
