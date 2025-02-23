6fcd4de23c7509d635524f46a30ca3903b73f596
Cosmin <cosminmorari99@yahoo.com>
2025-02-23 15:22:27 +0200

    asd

--- app/Http/Controllers/SuperAdmin/EmailsController.php
+++ app/Http/Controllers/SuperAdmin/EmailsController.php
@@ -41,7 +41,7 @@ class EmailsController extends Controller
         // dd($request->email);
         $mailAdressView = $request->email;
         $owner = User::where('role', 'owner')->first();
-        $messages = Email::where('id', $mailAdressView)->first();
+        $message = Email::where('id', $mailAdressView)->first();
 
         return view('superAdmin/view-email', compact('owner', 'message'));
     }
