95b33527f55882241d4029932cc752da14a3ae89
Cosmin <cosminmorari99@yahoo.com>
2025-02-24 11:28:34 +0200

    asd

--- resources/views/superAdmin/emails.blade.php
+++ resources/views/superAdmin/emails.blade.php
@@ -14,20 +14,65 @@
                 <ul class="nav nav-tabs" id="myTab" role="tablist">
                     <!-- Tabul pentru Toate mesajele -->
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Toate mesajele</button>
+                        <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home"
+                            type="button" role="tab" aria-controls="home" aria-selected="true">Toate mesajele</button>
                     </li>
                     <!-- Tabul pentru Mesaje necitite -->
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="unread-tab" data-bs-toggle="tab" data-bs-target="#unread" type="button" role="tab" aria-controls="unread" aria-selected="false">Mesaje Necitite</button>
+                        <button class="nav-link" id="unread-tab" data-bs-toggle="tab" data-bs-target="#unread"
+                            type="button" role="tab" aria-controls="unread" aria-selected="false">Mesaje
+                            Necitite</button>
                     </li>
                     <!-- Tabul pentru Mesaje Trimise -->
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="sent-tab" data-bs-toggle="tab" data-bs-target="#sent" type="button" role="tab" aria-controls="sent" aria-selected="false">Mesaje Trimise</button>
+                        <button class="nav-link" id="sent-tab" data-bs-toggle="tab" data-bs-target="#sent" type="button"
+                            role="tab" aria-controls="sent" aria-selected="false">Mesaje Trimise</button>
                     </li>
                     <!-- Buton pentru a compune un email, mutat în dreapta -->
+                    <!-- Butonul pentru a deschide modalul -->
                     <li class="ms-auto">
-                        <a href="#" class="btn btn-danger btn-sm" role="button"><i class="fa fa-edit"></i> Compune un mail</a>
+                        <a href="#" class="btn btn-danger btn-sm" role="button" data-bs-toggle="modal"
+                            data-bs-target="#composeEmailModal">
+                            <i class="fa fa-edit"></i> Compune un mail
+                        </a>
                     </li>
+
+                    <!-- Modalul pentru compunerea unui mail -->
+                    <div class="modal fade" id="composeEmailModal" tabindex="-1" aria-labelledby="composeEmailModalLabel"
+                        aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="composeEmailModalLabel">Compune un Mail</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <form id="emailForm">
+                                        <div class="mb-3">
+                                            <label for="recipient" class="form-label">Destinatar</label>
+                                            <input type="email" class="form-control" id="recipient"
+                                                placeholder="Introduceti destinatarul">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="subject" class="form-label">Subiect</label>
+                                            <input type="text" class="form-control" id="subject"
+                                                placeholder="Introduceti subiectul">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="message" class="form-label">Mesaj</label>
+                                            <textarea class="form-control" id="message" rows="4" placeholder="Introduceti mesajul"></textarea>
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="attachment" class="form-label">Atașament</label>
+                                            <input type="file" class="form-control" id="attachment">
+                                        </div>
+                                        <button type="submit" class="btn btn-primary">Trimite</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </ul>
 
                 <!-- Contul pentru taburi -->
@@ -97,7 +142,8 @@
                                                 </span>
                                             </td>
                                             <td>
-                                                <span class="subject {{ $email->is_seen ? 'seen' : 'text-dark fw-bold' }}">
+                                                <span
+                                                    class="subject {{ $email->is_seen ? 'seen' : 'text-dark fw-bold' }}">
                                                     {{ $email->subject }}
                                                 </span>
                                             </td>
@@ -138,7 +184,8 @@
                                                 </span>
                                             </td>
                                             <td>
-                                                <span class="subject {{ $email->is_seen ? 'seen' : 'text-dark fw-bold' }}">
+                                                <span
+                                                    class="subject {{ $email->is_seen ? 'seen' : 'text-dark fw-bold' }}">
                                                     {{ $email->subject }}
                                                 </span>
                                             </td>
