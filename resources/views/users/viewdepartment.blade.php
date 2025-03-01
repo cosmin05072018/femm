700167202c138394c16c9b6f606dcd17c8527e7e
Cosmin <cosminmorari99@yahoo.com>
2025-03-01 15:08:22 +0200

    asd

--- resources/views/users/viewdepartment.blade.php
+++ resources/views/users/viewdepartment.blade.php
@@ -162,10 +162,6 @@
             </div>
         </div>
     </div>
-
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script>
         console.log("jQuery loaded");
         $(document).ready(function() {
@@ -174,7 +170,7 @@
                 pageLength: 5
             });
 
-            console.log($("#add-row"));
+            {{-- console.log($("#add-row")); --}}
 
             // Definim butoanele de acțiune
             var action =
