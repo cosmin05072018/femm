6ef8547582c226059ef2537dfa9b3820aab1e9f0
Cosmin <cosminmorari99@yahoo.com>
2025-03-02 14:02:22 +0200

    asasdasdd

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,3 +12,4 @@ import ChatComponent from './components/ChatComponent.vue';
 const app = createApp({});
 app.component('chat-component', ChatComponent);
 app.mount('#app');
+console.log('Vue app mounted');
