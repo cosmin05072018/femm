7eb22570455b235d1f35429baf477cd5c8b444d9
Cosmin <cosminmorari99@yahoo.com>
2025-03-02 14:35:41 +0200

    asd

--- resources/views/users/chat.blade.php
+++ resources/views/users/chat.blade.php
@@ -3,9 +3,8 @@
 @section('title', 'Chat')
 
 @section('content-users')
-
-<h1>hello</h1>
     <div id="app">
+        <h1>hello chat</h1>
         <chat-component></chat-component>
     </div>
 @endsection
