205ee15385a8289fe17b82e5110aa0deee2ba136
Cosmin <cosminmorari99@yahoo.com>
2025-03-02 15:22:01 +0200

    asd

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,4 @@
 const mix = require('laravel-mix');
 
-// Compilează fișierul JavaScript
 mix.js('resources/js/app.js', 'public/js')
-
-// Compilează fișierul CSS
-   .sass('resources/sass/app.scss', 'public/css')
-
-// Generează un fișier manifest pentru cache-busting
-   .version(); // Opțional: Folosește .version() pentru a genera fișiere cu hash pentru cache-busting
+   .sass('resources/sass/app.scss', 'public/css');
