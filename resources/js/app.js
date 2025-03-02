cb58268f1425fa228721258cfd867f05947db90b
Cosmin <cosminmorari99@yahoo.com>
2025-03-02 15:52:55 +0200

    asd

--- resources/js/app.js
+++ resources/js/app.js
@@ -24,3 +24,4 @@ import ChatComponent from './components/ChatComponent.vue';
 const app = createApp({});
 app.component('chat-component', ChatComponent);
 app.mount('#app');
+
--- resources/views/users/chat.blade.php
+++ resources/views/users/chat.blade.php
@@ -3,6 +3,9 @@
 @section('title', 'Chat')
 
 @section('content-users')
+
+@vite(['resources/js/app.js'])
+
     <div id="app">
         <h1>hello</h1>
         <chat-component></chat-component>
