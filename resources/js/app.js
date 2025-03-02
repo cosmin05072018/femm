5305aaf3a1eceb9632f08a77370b7915fc375430
femmro <femmro@athena.hosterion.net>
2025-03-02 14:47:06 +0200

    asd

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,15 @@ window.Alpine = Alpine;
 
 Alpine.start();
 
+window.Pusher = require('pusher-js');
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    encrypted: true,
+});
+
+
 import { createApp } from 'vue';
 import ChatComponent from './components/ChatComponent.vue';
 
