e54ebdb074bb62ac0352a3c58aeba7e46923b8a5
Cosmin <cosminmorari99@yahoo.com>
2025-03-02 12:55:35 +0200

    asd

--- resources/views/layouts/userslayout.blade.php
+++ resources/views/layouts/userslayout.blade.php
@@ -34,6 +34,7 @@
     <link href="{{ asset('css/kaiadmin.min.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
+    @vite(['resources/js/app.js'])
 
     <!-- =======================================================
   * Template Name: Impact
