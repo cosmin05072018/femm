425024afc3a304995e401def3cf0f8b537722e4d
Cosmin <cosminmorari99@yahoo.com>
2025-03-23 17:17:14 +0200

    asdASD

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,23 +5,3 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
-
-
-import Echo from 'laravel-echo';
-window.Pusher = require('pusher-js');
-
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: '4126cc215b26910f04eb',
-    cluster: 'eu',
-    encrypted: true,
-});
-
-
-import { createApp } from 'vue';
-import ChatComponent from './components/ChatComponent.vue';
-
-const app = createApp({});
-app.component('chat-component', ChatComponent);
-app.mount('#app');
-
