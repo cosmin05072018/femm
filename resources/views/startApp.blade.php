fdee815d955e80b41b2cacfb93a701493a4fe9f1
Cosmin <cosminmorari99@yahoo.com>
2025-03-23 17:20:02 +0200

    asdASD

--- resources/views/startApp.blade.php
+++ resources/views/startApp.blade.php
@@ -860,8 +860,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="{{ asset('css/custom-design.css') }}" rel="stylesheet">
-
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.0.24/dist/tailwind.min.css" rel="stylesheet">
 </head>
 
 <body>
