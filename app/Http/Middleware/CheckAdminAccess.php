612244c5684545e2da07b9613e96d6b4fc7ae793
Cosmin <cosminmorari99@yahoo.com>
2025-06-21 23:00:46 +0300

    ad

--- app/Http/Middleware/CheckAdminAccess.php
+++ app/Http/Middleware/CheckAdminAccess.php
@@ -17,29 +17,29 @@ class CheckAdminAccess
      */
     public function handle(Request $request, Closure $next)
     {
-        $user = Auth::user();
+        // $user = Auth::user();
 
-        // Verificăm dacă utilizatorul este autentificat și are status = 1
-        if (!$user || $user->status !== 1) {
-            return redirect('fantastic-admin');
-        }
+        // // Verificăm dacă utilizatorul este autentificat și are status = 1
+        // if (!$user || $user->status !== 1) {
+        //     return redirect('fantastic-admin');
+        // }
 
-        // Verificăm ruta curentă
-        $currentRoute = $request->path();
+        // // Verificăm ruta curentă
+        // $currentRoute = $request->path();
 
-        // Reguli pentru redirecționare în funcție de rol_id
-        if ($currentRoute === 'fantastic-admin') {
-            if (in_array($user->role_id, [2, 3, 4])) { // super-admin (2) sau admin (3)
-                return redirect('fantastic-admin/management-hotel');
-            } elseif ($user->role_id !== 1) { // Nu este owner (1)
-                return redirect('fantastic-admin')->with('error', 'Acces permis doar pentru owner.');
-            }
-        }
+        // // Reguli pentru redirecționare în funcție de rol_id
+        // if ($currentRoute === 'fantastic-admin') {
+        //     if (in_array($user->role_id, [2, 3, 4])) { // super-admin (2) sau admin (3)
+        //         return redirect('fantastic-admin/management-hotel');
+        //     } elseif ($user->role_id !== 1) { // Nu este owner (1)
+        //         return redirect('fantastic-admin')->with('error', 'Acces permis doar pentru owner.');
+        //     }
+        // }
 
-        if ($currentRoute === 'fantastic-admin/management-hotel' && !in_array($user->role_id, [2, 3, 4])) {
-            return redirect('fantastic-admin')->with('error', 'Acces permis doar pentru super-admin sau admin.');
-        }
+        // if ($currentRoute === 'fantastic-admin/management-hotel' && !in_array($user->role_id, [2, 3, 4])) {
+        //     return redirect('fantastic-admin')->with('error', 'Acces permis doar pentru super-admin sau admin.');
+        // }
 
-        return $next($request);
+        // return $next($request);
     }
 }
