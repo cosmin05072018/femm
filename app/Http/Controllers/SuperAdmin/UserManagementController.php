4fb693a92b2844225f023b57ad7378ea52a96131
Cosmin <cosminmorari99@yahoo.com>
2025-06-21 23:08:45 +0300

    ad

--- app/Http/Controllers/SuperAdmin/UserManagementController.php
+++ app/Http/Controllers/SuperAdmin/UserManagementController.php
@@ -28,9 +28,9 @@ class UserManagementController extends Controller
     {
         // Preluăm toți utilizatorii din baza de date
         $users = User::all();
-        $owner = auth()->user()->role_id === 1 ? auth()->user() : null;
+        $owner = auth()->user()->role_id === '1' ? auth()->user() : null;
 
-        dd(auth()->user()->role_id === '1');
+        // dd(auth()->user()->role_id === '1');
 
         // Returnăm view-ul cu lista utilizatorilor
         return view('superAdmin/usermanagement', compact('users', 'owner'));
